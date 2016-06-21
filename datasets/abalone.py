040f21efb2d135764872af9ce1f45265e68d7f18
Lukas Krenz <lukas@krenz.land>
2016-06-21 21:49:35 +0200

    Tidy abalone dataset.

--- datasets/abalone.py
+++ datasets/abalone.py
@@ -14,23 +14,29 @@ def main():
     names = ['sex', 'length' , 'diameter', 'height', 'whole_weight', 'shucked_weight',
              'viscera_weight', 'shell_weight', 'rings']
     df = pd.read_csv('../datasets/raw/abalone/abalone.data.txt', names=names)
+
+    # Fix obvious data entry errors
+    df = df[df.height != 0]
+    ix = df[df.height == 1.13].index
+    df = df.set_value(ix, 'height', 0.130) # abalone is rather small, 0.13 seems plausible
+
     df = pd.get_dummies(df)
     cols = ['length', 'diameter', 'height', 'whole_weight', 'shucked_weight',
             'viscera_weight', 'shell_weight', 'sex_F', 'sex_I', 'sex_M', 'rings']
     df = df[cols] # reorder columns after dummies, target is always last!
-    df = df[cols]
     _, df = scale(df)
 
-    lambdas = { 'diameter': 3.2163821798943193,
-                'height': -1.9914054656190996,
-                'length': 3.3648670820916347,
+    lambdas = {'diameter': 3.2194196832186464,
+                'height': 1.2086273383776132,
+                'length': 3.3682627709337996,
+                'rings': 1,
                 'sex_F': 1,
                 'sex_I': 1,
                 'sex_M': 1,
-                'shell_weight': -1.1448450483002799,
-                'shucked_weight': -1.4107540627818103,
-                'viscera_weight': -1.2542787866665741,
-                'whole_weight': -0.76682358269227757}
+                'shell_weight': -1.1449045766430477,
+                'shucked_weight': -1.4088820347405902,
+                'viscera_weight': -1.2520473863735537,
+                'whole_weight': -0.7649280664662178}
 
     df = transform_cox(df, lambdas)
     df.to_csv(output_csv, index=False)
